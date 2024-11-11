4160c6f121dda11d97434a675686ad98567e948c
unknown <jhonny-529@outlook.com>
2024-11-11 11:56:55 -0500

    cambios2

--- sistema/model/UsuarioModel/AgregarUsuarioModel.php
+++ sistema/model/UsuarioModel/AgregarUsuarioModel.php
@@ -3,27 +3,23 @@ require_once __DIR__ . '/../conexion.php';
 
 class AgregarUsuarioModel extends Database{}
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Obtener los datos del formulario
+
     $dni = $_POST['dni'];
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $horario = $_POST['horario'];
     $puesto = $_POST['puesto'];
 
-    // Crear la consulta SQL para insertar el nuevo usuario
     $sql = "INSERT INTO usuarios (DNI, nombre, apellido, horario, puesto) VALUES (:dni, :nombre, :apellido, :horario, :puesto)";
 
-    // Preparar la consulta
     $stmt = $conn->prepare($sql);
 
-    // Vincular los parámetros
     $stmt->bindParam(':dni', $dni);
     $stmt->bindParam(':nombre', $nombre);
     $stmt->bindParam(':apellido', $apellido);
     $stmt->bindParam(':horario', $horario);
     $stmt->bindParam(':puesto', $puesto);
 
-    // Ejecutar la consulta
     if ($stmt->execute()) {
         echo "Usuario creado exitosamente.";
     } else {
