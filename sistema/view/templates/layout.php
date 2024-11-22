8a8c30efed5013f6d20a1c55e3a2ea8ffb02d72e
Jhonny <153967423+JhonnySAVAL@users.noreply.github.com>
2024-11-22 11:41:39 -0500

    Cambios locales en layout.php

--- sistema/view/templates/layout.php
+++ sistema/view/templates/layout.php
@@ -158,35 +158,16 @@
                                 <p>Feriados <i class="nav-arrow bi bi-chevron-right"></i></p>
                             </a>
                             <ul class="nav nav-treeview">
-                                <!-- <li class="nav-item">
-                                    <a href="/biometrico/sistema/controller/ConfiguracionesController/FeriadosController.php?action=MostrarFeriadosPorAno" class="nav-link">
-                                        <i class="nav-icon bi bi-calendar-day"></i>
-                                        <p>Feriados por Año</p>
-                                    </a>
-                                </li> -->
+
                                 <li class="nav-item">
-                                    <a href="/biometrico/sistema/controller/ConfiguracionesController/FeriadosController.php?action=CrearFeriado" class="nav-link">
+                                    <a href="/biometrico/sistema/controller/ConfiguracionesController/FeriadosController.php?action=mostrarFeriados" class="nav-link">
                                         <i class="nav-icon bi bi-calendar-day"></i>
-                                        <p>Crear Feriado</p>
+                                        <p>Ver Feriados</p>
                                     </a>
                                 </li>
                             </ul>
                         </li>
 
-                        <!-- <li class="nav-item menu-close">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon bi bi-file-earmark"></i>
-                                <p>Reportes <i class="nav-arrow bi bi-chevron-right"></i></p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="/biometrico/sistema/controller/AsistenciasController/ReportesController.php?action=generarReporteGeneral" class="nav-link">
-                                        <i class="nav-icon bi bi-file-earmark-text"></i>
-                                        <p>Ver Reportes</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li> -->
 
                         <li class="nav-item menu-close">
                             <a href="#" class="nav-link">
@@ -242,8 +223,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha256-whL0tQWoY1Ku1iskqPFvmZ+CHsvmRWx/PIoEvIeWh4I=" crossorigin="anonymous"></script> <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script> <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
     <script src="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.min.js" integrity="sha256-+vh8GkaU7C9/wbSLIcwq82tQ2wTf44aOHA8HlBMwRI8=" crossorigin="anonymous"></script>
-<!-- Agregar Bootstrap CSS en la sección <head> -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 <!-- Agregar Bootstrap JS y jQuery antes de cerrar el </body> -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -252,146 +231,7 @@
     <script src="../../public/js/sweetalert2@11.all.min.js"></script>
     <script src="../../public/js/logout.min.js"></script>
     <script src="../../resources/dist/js/adminlte.js"></script>
-    <!-- <script>
-            const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
-            const Default = {
-                scrollbarTheme: "os-theme-light",
-                scrollbarAutoHide: "leave",
-                scrollbarClickScroll: true,
-            };
-            document.addEventListener("DOMContentLoaded", function() {
-                const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
-                if (
-                    sidebarWrapper &&
-                    typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined"
-                ) {
-                    OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
-                        scrollbars: {
-                            theme: Default.scrollbarTheme,
-                            autoHide: Default.scrollbarAutoHide,
-                            clickScroll: Default.scrollbarClickScroll,
-                        },
-                    });
-                }
-            });
-        </script> 
-        <script>
-            
-
-            const visitors_chart_options = {
-                series: [{
-                        name: "High - 2023",
-                        data: [100, 120, 170, 167, 180, 177, 160],
-                    },
-                    {
-                        name: "Low - 2023",
-                        data: [60, 80, 70, 67, 80, 77, 100],
-                    },
-                ],
-                chart: {
-                    height: 200,
-                    type: "line",
-                    toolbar: {
-                        show: false,
-                    },
-                },
-                colors: ["#0d6efd", "#adb5bd"],
-                stroke: {
-                    curve: "smooth",
-                },
-                grid: {
-                    borderColor: "#e7e7e7",
-                    row: {
-                        colors: ["#f3f3f3", "transparent"], 
-                        opacity: 0.5,
-                    },
-                },
-                legend: {
-                    show: false,
-                },
-                markers: {
-                    size: 1,
-                },
-                xaxis: {
-                    categories: ["22th", "23th", "24th", "25th", "26th", "27th", "28th"],
-                },
-            };
-
-            const visitors_chart = new ApexCharts(
-                document.querySelector("#visitors-chart"),
-                visitors_chart_options
-            );
-            visitors_chart.render();
-
-            const sales_chart_options = {
-                series: [{
-                        name: "Net Profit",
-                        data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-                    },
-                    {
-                        name: "Revenue",
-                        data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-                    },
-                    {
-                        name: "Free Cash Flow",
-                        data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
-                    },
-                ],
-                chart: {
-                    type: "bar",
-                    height: 200,
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        columnWidth: "55%",
-                        endingShape: "rounded",
-                    },
-                },
-                legend: {
-                    show: false,
-                },
-                colors: ["#0d6efd", "#20c997", "#ffc107"],
-                dataLabels: {
-                    enabled: false,
-                },
-                stroke: {
-                    show: true,
-                    width: 2,
-                    colors: ["transparent"],
-                },
-                xaxis: {
-                    categories: [
-                        "Feb",
-                        "Mar",
-                        "Apr",
-                        "May",
-                        "Jun",
-                        "Jul",
-                        "Aug",
-                        "Sep",
-                        "Oct",
-                    ],
-                },
-                fill: {
-                    opacity: 1,
-                },
-                tooltip: {
-                    y: {
-                        formatter: function(val) {
-                            return "$ " + val + " thousands";
-                        },
-                    },
-                },
-            };
-
-            const sales_chart = new ApexCharts(
-                document.querySelector("#sales-chart"),
-                sales_chart_options
-            );
-            sales_chart.render();
-        </script> -->
-
+   
     <!-- Scripts adicionales -->
     <?php if (!empty($additionalJs)) : ?>
         <?php foreach ($additionalJs as $jsFile) : ?>
